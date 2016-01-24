d05c71238ddbd90a6fe52fc76fd887fa12575ec5
axdg <axdg@dfant.asia>
2016-01-25 02:14:21 +1100

    initial

---
+++ index.js
@@ -0,0 +1,9 @@
+// var Benchmark = require('benchmark');
+// var yaml = require('js-yaml');
+
+// need to implement:
+  // large front-matter block
+  // small front-matter block
+  // empty
+  // no front matter
+  // no body
